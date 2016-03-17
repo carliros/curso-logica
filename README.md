dad52c8a5b2662172a778009734f063dd60a6743
Carlos Gomez <carliros.g@gmail.com>
2016-03-17 17:13:46 -0400

    adding initials

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # curso-logica
-Herramientas del curso de lógica
+
+Herramientas del curso de lógica, inicialmente los ejercicios.
+
+Nota.- el código del motor base para los ejercicios no es propio, los tomé prestado del curso 
+[Introduction to Logic from Stanford University](http://arrogant.stanford.edu/intrologic/).
+
---
+++ src/Main.elm
@@ -0,0 +1,17 @@
+import Markdown
+
+main =
+  Markdown.toHtml markdown
+
+
+markdown = """
+
+# Curso de Introducción a la Lógica
+
+## Sistema Mendelson - Ejercicios
+
+  * Ejercicio 1
+
+Nota.- El código del motor base para los ejercicios no es propio, los
+tomé prestado del curso [Introduction to Logic from Stanford University](http://arrogant.stanford.edu/intrologic/).
+"""
